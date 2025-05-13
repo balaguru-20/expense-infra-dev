8f94fc1d51e82dd9967281b054d0feb7072f2a82
balaguru-20 <balaguravaiah0606@gmail.com>
2025-05-13 15:35:24 +0530

    security group for expense infra

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -37,3 +37,33 @@ module "bastion_sg" {
   vpc_id         = data.aws_ssm_parameter.vpc_id.value
   common_tags    = var.common_tags
 }
+
+module "app_alb_sg" {
+  source         = "git::https://github.com/balaguru-20/terraform-aws-securitygroup.git?ref=main"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_name        = "app_alb_sg"
+  sg_description = "Created for backend ALB instances in expense dev"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  common_tags    = var.common_tags
+}
+
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion_sg.sg_id
+  security_group_id        = module.app_alb_sg.sg_id
+}
+
+#To connect with ec2 instance which is tagged with this security group we need to add this rule
+resource "aws_security_group_rule" "bastion_public" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  # cidr_blocks = ["152.59.202.44/32"] # My laptop ip
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
