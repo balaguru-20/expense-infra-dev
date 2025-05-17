9a2d4dfdb35b0685ddb757f4ded6de74e70c1694
balaguru-20 <balaguravaiah0606@gmail.com>
2025-05-17 23:48:42 +0530

    terraform expense infra

---
+++ 90-frontend/frontend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dnf install ansible -y
+#This is for push
+# ansible-playbook -i inventory mysql.yaml
+
+#This is for pull
+ansible-pull -i localhost, -U https://github.com/balaguru-20/expense-ansible-roles-tf.git main.yaml -e COMPONENT=frontend -e ENVIRONMENT=$1
